51ccb8792aa49337258f525adf75e6887a00287a
Andrea Bartolucci <8514491+andbartol@users.noreply.github.com>
2025-07-26 11:00:49 +0200

    Added rules.mk

---
+++ andbartol/rules.mk
@@ -0,0 +1,11 @@
+# Build Options
+#   change yes to no to disable
+#
+
+RGBLIGHT_SUPPORTED = no
+RGB_MATRIX_SUPPORTED = no
+CONSOLE_ENABLE = no
+TAP_DANCE_ENABLE = yes
+MOUSEKEY_ENABLE = yes
+
+DIGITIZER_ENABLE = yes
